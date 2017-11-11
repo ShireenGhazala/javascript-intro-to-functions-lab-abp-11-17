22840c48afac92eaae4eb28321210486a8db67b2
Shireen Ghazala <shireen.gaz@gmail.com>
2017-11-11 08:19:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function sayHiToGrandma(string) {
   else { if (string.toUpperCase===string){
           hello= "YES INDEED!"
         }}
-  else {
+  else { if (string==="I love you, Grandma."){
     hello="I love you, too."
-      }
+  }}
     return hello
 }
